315b24d8aaa5f62f6d8c545fb2b223842bd08007
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-11-26 22:09:23 -0300

    Add static media URL handling to URL configuration

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -1,17 +1,17 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
-from django.contrib import admin
-
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('servicios/', views.servicios, name='servicios'),
-    path('proyectos/', views.proyectos, name='proyectos'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('servicio/<int:id>/', views.detalle_servicio, name='detalle_servicio'),
-    path('proyecto/<int:id>/', views.detalle_proyecto, name='detalle_proyecto'),
-    path('buscar/', views.buscar, name='buscar'),
-    path('admin/', admin.site.urls), 
-    
-]
+    path("", views.home, name="home"),
+    path("servicios/", views.servicios, name="servicios"),
+    path("proyectos/", views.proyectos, name="proyectos"),
+    path("contacto/", views.contacto, name="contacto"),
+    path("servicio/<int:id>/", views.detalle_servicio, name="detalle_servicio"),
+    path("proyecto/<int:id>/", views.detalle_proyecto, name="detalle_proyecto"),
+    path("buscar/", views.buscar, name="buscar"),
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
