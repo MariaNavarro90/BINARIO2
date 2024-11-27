566b48c80c14da20e79bd2c38aa0f7b0c5b9c32f
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-11-26 21:18:37 -0300

    Remove unused GitHub proyectos API endpoint from URLs

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import github_proyectos
 from django.contrib import admin
 
 
@@ -13,7 +12,6 @@ urlpatterns = [
     path('servicio/<int:id>/', views.detalle_servicio, name='detalle_servicio'),
     path('proyecto/<int:id>/', views.detalle_proyecto, name='detalle_proyecto'),
     path('buscar/', views.buscar, name='buscar'),
-    path("api/github/proyectos", github_proyectos, name="github_proyectos"),
     path('admin/', admin.site.urls), 
     
 ]
