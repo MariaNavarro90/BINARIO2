feff3faccc899c9469a45d836b3a95a203a0bfbf
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-11-26 22:09:13 -0300

    Add media URL and root settings in Django configuration

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Proyecto1/static'),
 ]
 
 # Default primary key field type
