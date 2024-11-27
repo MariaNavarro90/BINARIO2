091803204d7b532ed5677cfe2ae79a7c6fb05d4a
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-11-26 23:48:00 -0300

    Add custom message tags for Django messaging framework

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,7 +72,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Proyecto1.wsgi.application'
 
-
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'error',
+}
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
