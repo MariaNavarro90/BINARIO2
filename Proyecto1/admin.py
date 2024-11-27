fa8944ac1ff5eaed9cfd5e5829a53da05c87b0b2
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-11-26 22:09:05 -0300

    Register Testimonio model in admin panel

--- Proyecto1/admin.py
+++ Proyecto1/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Servicio, Proyecto, Consulta
+from .models import Servicio, Proyecto, Consulta, Testimonio
 
 admin.site.register(Servicio)
 admin.site.register(Proyecto)
 admin.site.register(Consulta)
+admin.site.register(Testimonio)
